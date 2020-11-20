7caa06aac04c3d22b408bcbac15f948dad8c02dd
Your Name <you@example.com>
2020-11-20 17:34:16 +0800

    test a looper count-down timer dialog

--- demo-timer-countdown-looper/app/src/main/java/com/example/timerlooper/MainActivity.java
+++ demo-timer-countdown-looper/app/src/main/java/com/example/timerlooper/MainActivity.java
@@ -1,11 +1,11 @@
 package com.example.timerlooper;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Random;
 
@@ -15,74 +15,77 @@ import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.core.ObservableEmitter;
 import io.reactivex.rxjava3.core.ObservableOnSubscribe;
 import io.reactivex.rxjava3.core.Scheduler;
-import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
 import io.reactivex.rxjava3.functions.Action;
 import io.reactivex.rxjava3.functions.Consumer;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
-    private ProcessStateDialog mProcessStateDialog;
     private final int mTimeoutSec = 10;
-    private Disposable mDisposable;
+    private ProcessStateDialog mProcessStateDialog;
     private Scheduler mScheduler;
+    private CompositeDisposable mCompositeDisposable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mScheduler = Schedulers.newThread();
+        mCompositeDisposable = new CompositeDisposable();
         mProcessStateDialog = new ProcessStateDialog(this);
 
         Button button = findViewById(R.id.hello_btn);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mDisposable = Observable.create(new ObservableOnSubscribe<Integer>() {
-                    @Override
-                    public void subscribe(@NonNull ObservableEmitter<Integer> emitter) throws Throwable {
-                        Random random = new Random();
-                        random.setSeed(10);
-                        LogUtils.d("[onClick] showProgressDialog Observable: thread= "
-                                + Thread.currentThread().getId());
-                        for (int i = 0; i < 50; i++) {
-                            int r = random.nextInt(5);
-                            if (r > 0) {
-                                SystemClock.sleep(r * 1000);
+                mCompositeDisposable.add(
+                        Observable.create(new ObservableOnSubscribe<Integer>() {
+                            @Override
+                            public void subscribe(@NonNull ObservableEmitter<Integer> emitter) throws Throwable {
+                                Random random = new Random();
+                                random.setSeed(10);
+                                LogUtils.d("[onClick] showProgressDialog Observable: thread= "
+                                        + Thread.currentThread().getId());
+                                for (int i = 0; i < 50 && !mCompositeDisposable.isDisposed(); i++) {
+                                    int r = random.nextInt(5);
+                                    if (r > 0) {
+                                        SystemClock.sleep(r * 1000);
+                                        if (!emitter.isDisposed() && !mCompositeDisposable.isDisposed()) {
+                                            emitter.onNext(r);
+                                        }
+                                    }
+                                }
+
                                 if (!emitter.isDisposed()) {
-                                    emitter.onNext(r);
+                                    emitter.onComplete();
                                 }
                             }
-                        }
-
-                        if (!emitter.isDisposed()) {
-                            emitter.onComplete();
-                        }
-                    }
-                }).subscribeOn(mScheduler)
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Consumer<Integer>() {
-                    @Override
-                    public void accept(Integer aInteger) throws Throwable {
-                        mProcessStateDialog.setState(ProcessStateView.State.STATE_PROCESSING,
-                                "hello: " + aInteger, null
-                                , true, mTimeoutSec * 1000);
-                        if (!mProcessStateDialog.isShowing()) {
-                            mProcessStateDialog.show();
-                        }
-                    }
-                }, new Consumer<Throwable>() {
-                    @Override
-                    public void accept(Throwable throwable) throws Throwable {
-                        //nothing
-                    }
-                }, new Action() {
-                    @Override
-                    public void run() throws Throwable {
-                        if (mProcessStateDialog.isShowing()) {
-                            mProcessStateDialog.cancel();
-                        }
-                    }
-                });
+                        })
+                                .subscribeOn(mScheduler)
+                                .observeOn(AndroidSchedulers.mainThread())
+                                .subscribe(new Consumer<Integer>() {
+                                    @Override
+                                    public void accept(Integer aInteger) throws Throwable {
+                                        mProcessStateDialog.setState(ProcessStateView.State.STATE_PROCESSING,
+                                                "hello: " + aInteger, null
+                                                , true, mTimeoutSec * 1000);
+                                        if (!mCompositeDisposable.isDisposed() && !mProcessStateDialog.isShowing()) {
+                                            mProcessStateDialog.show();
+                                        }
+                                    }
+                                }, new Consumer<Throwable>() {
+                                    @Override
+                                    public void accept(Throwable throwable) throws Throwable {
+                                        //nothing
+                                    }
+                                }, new Action() {
+                                    @Override
+                                    public void run() throws Throwable {
+                                        if (mProcessStateDialog.isShowing()) {
+                                            mProcessStateDialog.cancel();
+                                        }
+                                    }
+                                }));
 
 
             }
@@ -92,8 +95,25 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStop() {
         super.onStop();
-        if (mDisposable != null && !mDisposable.isDisposed()) {
-            mDisposable.dispose();
+        if (mCompositeDisposable != null && !mCompositeDisposable.isDisposed()) {
+            mCompositeDisposable.clear();
         }
+
+        if (mProcessStateDialog.isShowing()) {
+            mProcessStateDialog.cancel();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mCompositeDisposable != null) {
+            mCompositeDisposable.clear();
+        }
+
+        /*if (mProcessStateDialog.isShowing()) {
+            mProcessStateDialog.cancel();
+        }*/
+        super.onBackPressed();
+
     }
 }
