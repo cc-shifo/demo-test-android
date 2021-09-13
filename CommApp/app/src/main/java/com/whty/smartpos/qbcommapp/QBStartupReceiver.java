d5151cde757ff8d2acb4b6fdfb5db9ad7cd72448
Your Name <you@example.com>
2021-09-13 17:15:42 +0800

    备份

---
+++ CommApp/app/src/main/java/com/whty/smartpos/qbcommapp/QBStartupReceiver.java
@@ -0,0 +1,22 @@
+package com.whty.smartpos.qbcommapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class QBStartupReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.i("QBStartupReceiver", "onReceive");
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+            Log.i("QBStartupReceiver", "onReceive: ACTION_BOOT_COMPLETED");
+            Intent i = new Intent(context, CommService.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startService(i);
+            // Intent i = new Intent(context, MainActivity.class);
+            // i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            // context.startActivity(i);
+        }
+    }
+}
