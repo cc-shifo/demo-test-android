01d2d504105d99e402c5c102e094bc7a69a66aa0
LiuJian <502091250@qq.com>
2022-02-09 08:37:31 +0800

    backup ftp demo(from github.com)

---
+++ AndroidFTP-master/app/src/main/java/com/sam/remote/activitiMain.java
@@ -0,0 +1,188 @@
+package com.sam.remote;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+
+public class activitiMain extends Activity {
+
+	  private final String sDir = "/public_html";
+	  private String[] array_spinner;
+	  ScrollView sv;
+	 
+	 
+		private final LinearLayout.LayoutParams LP_FF = new LinearLayout.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT);
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.main);
+        //loadLogs();
+    }
+	private void loadLogs() {
+		//radBtn.addView(null);
+		new processTask().execute();
+	}
+    private class processTask extends AsyncTask<String, Void, Void>{
+    	private final ProgressDialog Dialog = new ProgressDialog(activitiMain.this);
+    	 private TextView log; 
+    	 private  Spinner s;
+    	protected void onPreExecute() {
+    		Dialog.setMessage("Loading...");
+    		Dialog.show();
+     	}
+		@Override
+		protected Void doInBackground(String... arg0) {
+			FTPClient client = new FTPClient();
+	        try {
+	        SharedPreferences myPref = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+	   		String ipaddr = myPref.getString("etServer", "");
+	   		String port= myPref.getString("port", "21");
+	   		String logFile= myPref.getString("logfname", "error_log");
+	   		String filePath= myPref.getString("path", "/public_html");
+	   		String uname= myPref.getString("uname", "");
+	   		String pass= myPref.getString("pass", "");
+				ipaddr = "192.168.43.248";
+				try {
+					client.connect(ipaddr,21);
+				} catch (IOException e) {
+					e.printStackTrace();
+					return null;
+				}
+				client.enterLocalPassiveMode();
+	            boolean login = client.login("Anonymous", pass);
+	            client.changeWorkingDirectory(filePath);
+	            System.out.println(client.printWorkingDirectory());
+	          
+	            BufferedReader reader = null;
+	            String line = null;
+	            sv = new ScrollView(activitiMain.this);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
+					sv.setLayoutParams(LP_FF);
+					 LinearLayout loglay  =  new LinearLayout(activitiMain.this);
+					 loglay.setOrientation( LinearLayout.VERTICAL ); 
+	         //FTPFile[] ftpFiles = client.listFiles();
+					 FTPFile[] ftpFiles = client.listFiles();
+					 ArrayList<String> name = new ArrayList<String>();
+					 
+/*						         ArrayAdapter <CharSequence> adapter =
+	        	  new ArrayAdapter <CharSequence> (getBaseContext(), android.R.layout.simple_spinner_item );
+	        	adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+	        	
+	        	 adapter.add("Select File");*/      
+	        	//s = new (Spinner)findViewById(R.id.Spinner01);
+	        	s = new Spinner(activitiMain.this);
+	        	for (int i = 0; i < ftpFiles.length; i++) {	
+	        		String fname =ftpFiles[i].getName();
+	        		Log.i("FTP", "File " +i +" : "+fname);
+						   name.add(fname);
+						    long length = ftpFiles[i].getSize();
+						    //adapter2.add(ftpFiles[i].getName());
+						    //String readableLength = FileUtils.byteCountToDisplaySize( length );
+						    ///System.out.println( name + ":\t\t" + readableLength );  
+						}
+	        	//String [] strArray =null;
+	        	//strArray.toArray(name);
+	        	String [] files = name.toArray(new String[name.size()]);
+	        	ArrayAdapter adapter2 = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_spinner_item,files);  
+	             adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);  
+	             s.setAdapter(adapter2);  
+	             loglay.addView(s);
+	        	
+	         Log.i("Files", ftpFiles.toString());
+	            for (FTPFile ftpFile : ftpFiles) {
+	            	String fileName = ftpFile.getName(); 
+	            	if (fileName.equals(logFile)){
+	            		 try {
+	                         InputStream stream = client.retrieveFileStream(fileName);                             
+	                         reader = new BufferedReader(new InputStreamReader(stream,
+									 StandardCharsets.UTF_8));
+	                         //line = reader.readLine();
+	                        
+	         				
+	                         while ((line = reader.readLine())!= null && line.trim().length()>0) {
+	                        	    //System.out.println(line);
+	                        	 log = new TextView(activitiMain.this);
+	                        	    String trail = line; 
+	                        	    log.setTextColor(Color.GREEN);                                                                                                                                                                                                                                                                                                                                 
+	                                log.setPadding(10, 5, 0, 5);
+	                                log.setText(trail);                                                                                                                                                                                                                                                                                                                                   
+	                                loglay.addView(log);    
+	                        	}
+	                         sv.addView(loglay);
+	                     } finally {
+	                         if (reader != null) try { reader.close(); } catch (IOException logOrIgnore) {}
+	                     }
+	            	}
+	            }
+	               client.logout();
+	            
+	            
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        } finally {
+	            try {
+	                client.disconnect();
+	            } catch (IOException e) {
+	                e.printStackTrace();
+	            }
+	        }
+			return null;
+		}
+		protected void onPostExecute(Void unused) {
+    		Dialog.dismiss();
+    		setContentView(sv);
+    		}
+    }
+    // public class PCTailListener extends TailerListenerAdapter {
+    //     public void handle(String line) {
+    //         System.out.println(line);
+    //     }
+    // }
+    @SuppressLint("ResourceType")
+	@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mainmenu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){            	
+           switch (item.getItemId()) {
+        case R.id.refresh:            	
+        	loadLogs();
+        	return true;
+        case R.id.settings:
+        	Intent settingsActivity = new Intent(this, Preferences.class);
+        	startActivityForResult(settingsActivity,0);
+        default:
+            return super.onOptionsItemSelected(item);
+        }
+    }
+}
