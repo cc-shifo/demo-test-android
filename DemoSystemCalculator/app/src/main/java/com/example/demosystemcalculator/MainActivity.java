80725f9581695ee2f2d5baf7501cf0f4ffa97bae
cc-shifo <502091250@qq.com>
2024-11-29 16:17:06 +0800

    feature: 添加系统calculator2 APP. github路径: git clone https://github.com/aosp-mirror/platform_packages_apps_calculator.git
    cs.android.com路径：
    https://cs.android.com/android/platform/superproject/+/android-cts-7.0_r7:packages/apps/Calculator/

---
+++ DemoSystemCalculator/app/src/main/java/com/example/demosystemcalculator/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.demosystemcalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.mycalculator.Calculator;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.calculator);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, Calculator.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
